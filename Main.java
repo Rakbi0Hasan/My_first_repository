7cadcd1b4095424fe95199c32557ff280c65d8d2
rakib <hasankhanrakib0@gmail.com>
2024-11-15 00:10:38 +0600

    adding feature in new branch

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
+    public static void show(){
+        System.out.println("Rakib Hasan");
+    }
     public static void main(String args[]) {
         System.out.println("Hello world!");
         System.out.println("Doing some change.");
+        show();
     }
 }
